c72fb2092e39a8100b1f724699f2524478faba90
Noel Williams <1020854+ndwilliams3@users.noreply.github.com>
2022-11-04 09:29:31 -0500

    first commit

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import sanity from 'astro-sanity';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-sanity-minimal-starter.netlify.app/',
   // Your public domain, e.g.: https://my-site.dev/
-  integrations: [
-    sitemap(),
-    sanity({
-      projectId: '8hj1t7km',
-      dataset: 'production',
-      apiVersion: 'v2021-03-25',
-      useCdn: true,
-    }),
-  ],
+  integrations: [sitemap(), sanity({
+    projectId: 'htnr8xkf',
+    dataset: 'production',
+    apiVersion: 'v2021-03-25',
+    useCdn: true
+  }), tailwind({
+    config: { applyBaseStyles: false },
+  })]
 });
